6957c0f05ef96531e3fe9f886774d4a83a334acb
Noah Day <sheltielover432@gmail.com>
2020-09-21 23:15:38 -0400

    AI Brain

--- Doom/ai.py
+++ Doom/ai.py
@@ -1,7 +1,6 @@
 # AI for Doom
 
 
-
 # Importing the libraries
 import numpy as np
 import torch
@@ -20,5 +19,48 @@ import experience_replay, image_preprocessing
 
 
 # Part 1 - Building the AI
+# Brain
+class CNN(nn.Module):
+    def __init__(self, number_actions):
+        super(CNN, self).__init__()
+
+        # Convolution Layers
+        self.convolution1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5)
+        self.convolution2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3)
+        self.convolution3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=2)
+
+        #Full Connections
+        self.fc1 = nn.Linear(in_features=self.count_neurons((1, 80, 80)), out_features=40)
+        self.sc2 = nn.Linear(in_features=40, out_features=number_actions)
+
+    def count_neurons(self, image_dim):
+        #Create an image
+        x = Variable(torch.rand(1, *image_dim))
+
+        # Pass image through convolution layers
+        x = F.relu(F.max_pool2d(self.convolution1(x), 3, 2))
+        x = F.relu(F.max_pool2d(self.convolution2(x), 3, 2))
+        x = F.relu(F.max_pool2d(self.convolution3(x), 3, 2))
+
+        #Pass image through flattening layers
+        return x.data.view(1, -1).size(1)
+
+    def forward(self, x):
+        #Propogating the signals
+        x = F.relu(F.max_pool2d(self.convolution1(x), 3, 2))
+        x = F.relu(F.max_pool2d(self.convolution2(x), 3, 2))
+        x = F.relu(F.max_pool2d(self.convolution3(x), 3, 2))
+
+        #Flatten convolution layer
+        x = x.view(x.size(0), -1)
+
+        #Get hidden layer
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+
+        #Return output neurons
+        return x
+# Body
+
 
 # Part 2 - Training the AI with Deep Convolutional Q-Learning
